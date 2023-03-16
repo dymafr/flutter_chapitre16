34196735184b321566ff4d6cf2d49409a12931b8
Quentin <quentindevial19@gmail.com>
2023-03-16 18:45:33 +0100

    fix: merge

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const Trip = require("./models/trip.model");
 const multer = require("multer");
 const subpath = "/public/assets/images/activities";
 
-const subpath = "/public/assets/images/activities";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, subpath));
